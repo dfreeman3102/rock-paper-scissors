40b3412e6e0eb2e8372ecaeabe4ab42a3175ea21
dfreeman3102 <davidfreeman3102@gmail.com>
2024-02-15 20:52:58 -0500

    added basic layout for js

---
+++ script.js
@@ -0,0 +1,19 @@
+// prompt("Type R, P, S");
+
+// variables for rock, paper, scissors
+var R = "Rock";
+var P = "Paper";
+var S = "Scissors";
+
+// code for computer to choose
+
+
+
+
+// what happens depending on choice
+
+
+
+
+// alert("Type your Choice");
+// confirm("Do you want to play again?");
