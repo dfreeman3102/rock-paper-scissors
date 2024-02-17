6573bba93c4fd6e90ad2b086adc4db1af664b0af
dfreeman3102 <davidfreeman3102@gmail.com>
2024-02-17 11:52:34 -0500

    added choices if else

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// prompt("Type R, P, S");
+var input = prompt("Type R, P, S");
 
 // variables for rock, paper, scissors
-var R = "Rock";
-var P = "Paper";
-var S = "Scissors";
+var R = 1;
+var P = 2;
+var S = 3;
 
 // code for computer to choose
 
@@ -11,9 +11,23 @@ var S = "Scissors";
 
 
 // what happens depending on choice
+if (input === R && comp === P) {
+    console.log("Comp wins Comp chose paper");
+} else if (input === R && comp === S){
+    console.log("Player wins Comp chose scissors");
+} else if (input === P && comp === R){
+    console.log("Player wins Comp chose rock");
+} else if(input === P && comp === S){
+    console.log("Comp wins Comp chose scissors");
+} else if(input === S && comp === R){
+    console.log("Comp wins Comp chose rock");
+} else if(input === S && comp === P){
+    console.log("Player wins Comp chose Paper");
+} else {
+    console.log("Tie, both chose same item");
+}
 
 
 
-
-// alert("Type your Choice");
+// alert("Stats ");
 // confirm("Do you want to play again?");
