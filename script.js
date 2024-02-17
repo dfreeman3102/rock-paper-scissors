3b1ba6f3319b1ba92f60613857cf62ad85a7b3d8
dfreeman3102 <davidfreeman3102@gmail.com>
2024-02-17 12:46:19 -0500

    Added more info to the console for what happens depending on choice

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
+//User choice
 var input = prompt("Type R, P, S");
 
 // variables for rock, paper, scissors
-var R = 0;
-var P = 1;
-var S = 2;
+// var R = 0;
+// var P = 1;
+// var S = 2;
 
-// code for computer to choose
+// code for computer choice
 
-var comp = Math.floor(Math.random()*3);
-console.log(comp);
+var comp = Math.floor(Math.random() * 3);
 
 
 // what happens depending on choice
-if (input === R && comp === P) {
-    console.log("Comp wins Comp chose paper");
-} else if (input === R && comp === S){
-    console.log("Player wins Comp chose scissors");
-} else if (input === P && comp === R){
-    console.log("Player wins Comp chose rock");
-} else if(input === P && comp === S){
-    console.log("Comp wins Comp chose scissors");
-} else if(input === S && comp === R){
-    console.log("Comp wins Comp chose rock");
-} else if(input === S && comp === P){
-    console.log("Player wins Comp chose Paper");
+
+if(input ==="R"){
+    console.log("Player chose Rock");
+} else if (input ==="P"){
+ console.log("Player chose Paper");
+} else if (input ==="S"){
+    console.log("Player chose Scissors");
 } else {
-    console.log("Tie, both chose same item");
+    console.log("invalid choice");
+}
+
+if(comp === 0){
+    console.log("Computer chose Rock");
+} else if (comp === 1){
+ console.log("Computer chose Paper");
+} else if (comp === 2){
+    console.log("Computer chose Scissors");
+} 
+
+
+if (input === "R" && comp === 1) {
+    console.log("Computer wins");
+} else if (input === "R" && comp === 2){
+    console.log("Player wins");
+} else if (input === "P" && comp === 0){
+    console.log("Player wins");
+} else if(input === "P" && comp === 2){
+    console.log("Computer wins");
+} else if(input === "S" && comp === 0){
+    console.log("Computer wins");
+} else if(input === "S" && comp === 1){
+    console.log("Player wins");
+} else {
+    console.log("Tie, try again");
 }
 
 
